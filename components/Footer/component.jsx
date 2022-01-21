fd6412d3926fe818ae8b57e6b4c1e3a893e4206b
pedrohhcunha <pedro.cunha@onfinity.com.br>
2022-01-21 17:17:27 -0300

    subFooter from footer component

--- components/Footer/component.jsx
+++ components/Footer/component.jsx
@@ -10,12 +10,97 @@ export default function Footer(props){
     return(
         <footer className={styles.footer}>
             <div className={styles.subFooter}>
-
+                <div className={styles.resume}>
+                    <h5 className={styles.title}>Super Courses</h5>
+                    <p className={styles.paragraph}>Lorem ipsum, repudiandae dolorem ab unde corporis vel recusandae, labore consequuntur. Sapiente libero quibusdam labore.</p>
+                </div>
+                <div className={styles.groupInfos}>
+                    <div className={styles.areaLinks}>
+                        <h5 className={styles.title}>Company info</h5>
+                        <ul className={styles.list}>
+                            <li className={styles.item}>
+                                <Link target="_blank" href="https://www.google.com">
+                                    <a className={styles.link}>About us</a>
+                                </Link>
+                            </li>
+                            <li className={styles.item}>
+                                <Link target="_blank" href="https://www.google.com">
+                                    <a className={styles.link}>Carrier</a>
+                                </Link>
+                            </li>
+                            <li className={styles.item}>
+                                <Link target="_blank" href="https://www.google.com">
+                                    <a className={styles.link}>Blog</a>
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className={styles.areaLinks}>
+                        <h5 className={styles.title}>LGPD</h5>
+                        <ul className={styles.list}>
+                            <li className={styles.item}>
+                                <Link target="_blank" href="https://www.google.com">
+                                    <a className={styles.link}>About us</a>
+                                </Link>
+                            </li>
+                            <li className={styles.item}>
+                                <Link target="_blank" href="https://www.google.com">
+                                    <a className={styles.link}>Carrier</a>
+                                </Link>
+                            </li>
+                            <li className={styles.item}>
+                                <Link target="_blank" href="https://www.google.com">
+                                    <a className={styles.link}>Blog</a>
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className={styles.areaLinks}>
+                        <h5 className={styles.title}>Legal</h5>
+                        <ul className={styles.list}>
+                            <li className={styles.item}>
+                                <Link target="_blank" href="https://www.google.com">
+                                    <a className={styles.link}>About us</a>
+                                </Link>
+                            </li>
+                            <li className={styles.item}>
+                                <Link target="_blank" href="https://www.google.com">
+                                    <a className={styles.link}>Carrier</a>
+                                </Link>
+                            </li>
+                            <li className={styles.item}>
+                                <Link target="_blank" href="https://www.google.com">
+                                    <a className={styles.link}>Blog</a>
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className={styles.areaLinks}>
+                        <h5 className={styles.title}>Resources</h5>
+                        <ul className={styles.list}>
+                            <li className={styles.item}>
+                                <Link target="_blank" href="https://www.google.com">
+                                    <a className={styles.link}>About us</a>
+                                </Link>
+                            </li>
+                            <li className={styles.item}>
+                                <Link target="_blank" href="https://www.google.com">
+                                    <a className={styles.link}>Carrier</a>
+                                </Link>
+                            </li>
+                            <li className={styles.item}>
+                                <Link target="_blank" href="https://www.google.com">
+                                    <a className={styles.link}>Blog</a>
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </div>
             <div className={styles.finalBar}>
                 <Link target="_blank" href="https://www.google.com/">
                     <a className={styles.copyright}>
-                    All Rights Reserved. SuperCourses© 2022
+                    All Rights Reserved. Super Courses© 2022
                     </a>
                 </Link>
                 <div className={styles.socialMedias}>

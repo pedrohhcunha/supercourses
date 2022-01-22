0b350e0e18e528a3367f33cc26aeb475715b54f7
pedrohhcunha <pedro.cunha@onfinity.com.br>
2022-01-22 15:53:32 -0300

    Defined a title for a page

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,10 +5,14 @@ import Initial from "../components/Initial/component"
 import Header from "../components/Header/component"
 import LearnMore from "../components/LearnMore/component"
 import Plans from "../components/Plans/component"
+import Head from 'next/head'
 
 export default function Home() {
   return (
     <main>
+      <Head>
+        <title>Super Courses - for everything you need</title>
+      </Head>
       <Header />
       <Initial />
       <LearnMore />

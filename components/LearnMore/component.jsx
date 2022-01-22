e8741547f623253e260a35282e967d922d41fd97
pedrohhcunha <pedro.cunha@onfinity.com.br>
2022-01-22 11:22:30 -0300

    Added hover effect in link of learn more section

--- components/LearnMore/component.jsx
+++ components/LearnMore/component.jsx
@@ -2,6 +2,8 @@ import styles from './styles.module.scss'
 import Link from 'next/link'
 import Image from 'next/image'
 import image from './images/image.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAngleRight } from '@fortawesome/free-solid-svg-icons'
 
 export default function LearnMore(props){
     return(
@@ -10,7 +12,10 @@ export default function LearnMore(props){
                 <h2 className={styles.title}>Learn more with us</h2>
                 <p className={styles.paragraph}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Blanditiis corporis odio cumque omnis illo commodi. Quam maiores id, libero eos iure autem provident! Necessitatibus ad id adipisci sit expedita ullam!</p>
                 <Link href="https://google.com/" target="_blank">
-                    <a className={styles.link}>Learn more</a>
+                    <a className={styles.link}>
+                        <span>Learn more</span>
+                        <FontAwesomeIcon icon={faAngleRight} className={styles.icon} />
+                    </a>
                 </Link>
             </div>
             <div className={styles.areaImage}>

e44748ba1701c14f918a77fb4c1d4ed644531c1e
pedrohhcunha <pedro.cunha@onfinity.com.br>
2022-01-22 15:47:35 -0300

    Create a responsive side menu

--- components/Header/component.jsx
+++ components/Header/component.jsx
@@ -1,12 +1,14 @@
 import styles from './styles.module.scss'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Link from 'next/link'
-import { faArrowRight, faBars } from '@fortawesome/free-solid-svg-icons'
+import { faArrowRight, faBars, faTimes } from '@fortawesome/free-solid-svg-icons'
 import { useEffect, useState } from 'react';
 
 export default function Header (props){
 
     const [animateHeader, setAnimateHeader] = useState(false);
+
+    const [menuOpen, setMenuOpen] = useState(false);
 
     useEffect(() => {
         window.addEventListener("scroll", () => {
@@ -36,7 +38,28 @@ export default function Header (props){
                     <FontAwesomeIcon icon={faArrowRight} className={styles.icon} />
                 </button>
             </div>
-            <FontAwesomeIcon icon={faBars} className={styles.menuIcon} />
+            <FontAwesomeIcon icon={faBars} onClick={() => setMenuOpen(true)} className={styles.menuIcon} />
+            <menu className={`${styles.menu} ${menuOpen ? styles.active : ''}`}>
+                <div className={styles.areaClose}>
+                    <h4 className={styles.menuTitle}>Menu</h4>
+                    <FontAwesomeIcon icon={faTimes} onClick={() => setMenuOpen(false)} className={styles.iconClose}/>
+                </div>
+                <ul className={styles.list}>
+                    <li className={styles.item}>Home</li>
+                    <li className={styles.item}>Products</li>
+                    <li className={styles.item}>Pricing</li>
+                    <li className={styles.item}>Contact</li>
+                </ul>
+                <div className={styles.areaButtons}>
+                    <Link href="https://google.com/" target="_blank">
+                        <a className={styles.link}>Login</a>
+                    </Link>
+                    <button className={styles.button}>
+                        <span>Join us</span>
+                        <FontAwesomeIcon icon={faArrowRight} className={styles.icon} />
+                    </button>
+                </div>
+            </menu>
         </header>
     )
 }

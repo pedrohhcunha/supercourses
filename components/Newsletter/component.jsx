ff7aec95db9f8be1f86f0d819357d66845172b3c
pedrohhcunha <pedro.cunha@onfinity.com.br>
2022-01-22 12:13:14 -0300

    Added hover effect ans feed back in newsletter section

--- components/Newsletter/component.jsx
+++ components/Newsletter/component.jsx
@@ -1,6 +1,20 @@
+import { useState } from 'react';
 import styles from './styles.module.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
 export default function(props){
+
+    const [email, setEmail] = useState("");
+    const [emailSended, setEmailSended] = useState(false);
+
+    const submitForm = event => {
+        event.preventDefault();
+        console.log("Email:", email)
+        setEmail("")
+        setEmailSended(true)
+    }
+
     return(
         <section className={styles.section}>
             <h6 className={styles.subTitle}>Newsletter</h6>
@@ -9,10 +23,14 @@ export default function(props){
                 Problems trying to resolve the conflict between <br />
                 the two major realms of Classical physics: Newtonian mechanics </p>
 
-            <div className={styles.areaInput}>
-                <input className={styles.input} placeholder="Your better email" type="text" />
-                <button className={styles.button}>Subscribe</button>
-            </div>
+            <form onSubmit={event => submitForm(event)} className={styles.areaInput}>
+                <input onChange={event => setEmail(event.target.value)} value={email} name="email" className={styles.input} placeholder="Your better email" type="email" />
+                <button type='submit' className={styles.button}>
+                    <span>Subscribe</span>
+                    <FontAwesomeIcon icon={faArrowRight} className={styles.icon}/>
+                </button>
+            </form>
+            <div className={`${styles.feedback} ${emailSended ? styles.active : ''}`}>Enviamos um email de confirmação para <strong>phhcpedrohhcunha@gmail.com</strong>!</div>
         </section>
     )
 }

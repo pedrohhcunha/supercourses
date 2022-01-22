45b39fad0bab7b7063d8e3c917b99f3b3e75eb57
pedrohhcunha <pedro.cunha@onfinity.com.br>
2022-01-22 12:36:40 -0300

    Changethe text

--- components/Plans/component.jsx
+++ components/Plans/component.jsx
@@ -7,48 +7,17 @@ export default function Plans(props){
         <section className={styles.section}>
             <div className={styles.titleArea}>
                 <h2 className={styles.title}>Wacth our Super Courses</h2>
-                <p className={styles.paragraph}>Quam maiores id, libero eos iure autem provident! Necessitatibus ad id adipisci sit expedita ullam!</p>
+                <p className={styles.paragraph}>
+                    Get access to our super platform. <br />
+                    Learn what you want from wherever you want.
+                </p>
             </div>
             <div className={styles.cardsArea}>
                 <div className={styles.card}>
-                    <h3 className={styles.title}>Plano <strong>GOLD</strong></h3>
-                    <p className={styles.description}>Especial para universitários</p>
+                    <h3 className={styles.title}>Plan <strong>SILVER</strong></h3>
+                    <p className={styles.description}>To start learn today</p>
 
-                    <h4 className={styles.price}>$134</h4>
-
-                    <ul className={styles.list}>
-                        <li className={styles.item}>
-                            <div className={styles.areaIcon}>
-                                <FontAwesomeIcon icon={faCheck} className={styles.icon}/>
-                            </div>
-                            <span>1GB  Cloud storage</span>
-                        </li>
-                        <li className={styles.item}>
-                            <div className={styles.areaIcon}>
-                                <FontAwesomeIcon icon={faCheck} className={styles.icon}/>
-                            </div>
-                            <span>1GB  Cloud storage</span>
-                        </li>
-                        <li className={styles.item}>
-                            <div className={styles.areaIcon}>
-                                <FontAwesomeIcon icon={faCheck} className={styles.icon}/>
-                            </div>
-                            <span>1GB  Cloud storage</span>
-                        </li>
-                        <li className={styles.item}>
-                            <div className={styles.areaIcon}>
-                                <FontAwesomeIcon icon={faCheck} className={styles.icon}/>
-                            </div>
-                            <span>1GB  Cloud storage</span>
-                        </li>
-                    </ul>
-                    <button className={styles.button}>Try for free</button>
-                </div>
-                <div className={styles.card}>
-                    <h3 className={styles.title}>Plano <strong>GOLD</strong></h3>
-                    <p className={styles.description}>Especial para universitários</p>
-
-                    <h4 className={styles.price}>$134</h4>
+                    <h4 className={styles.price}>$139</h4>
 
                     <ul className={styles.list}>
                         <li className={styles.item}>
@@ -79,10 +48,44 @@ export default function Plans(props){
                     <button className={styles.button}>Try for free</button>
                 </div>
                 <div className={styles.card}>
-                    <h3 className={styles.title}>Plano <strong>GOLD</strong></h3>
-                    <p className={styles.description}>Especial para universitários</p>
+                    <h3 className={styles.title}>Plan <strong>GOLD</strong></h3>
+                    <p className={styles.description}>To update your mind with us</p>
 
-                    <h4 className={styles.price}>$134</h4>
+                    <h4 className={styles.price}>$179</h4>
+
+                    <ul className={styles.list}>
+                        <li className={styles.item}>
+                            <div className={styles.areaIcon}>
+                                <FontAwesomeIcon icon={faCheck} className={styles.icon}/>
+                            </div>
+                            <span>1GB  Cloud storage</span>
+                        </li>
+                        <li className={styles.item}>
+                            <div className={styles.areaIcon}>
+                                <FontAwesomeIcon icon={faCheck} className={styles.icon}/>
+                            </div>
+                            <span>1GB  Cloud storage</span>
+                        </li>
+                        <li className={styles.item}>
+                            <div className={styles.areaIcon}>
+                                <FontAwesomeIcon icon={faCheck} className={styles.icon}/>
+                            </div>
+                            <span>1GB  Cloud storage</span>
+                        </li>
+                        <li className={styles.item}>
+                            <div className={styles.areaIcon}>
+                                <FontAwesomeIcon icon={faCheck} className={styles.icon}/>
+                            </div>
+                            <span>1GB  Cloud storage</span>
+                        </li>
+                    </ul>
+                    <button className={styles.button}>Try for free</button>
+                </div>
+                <div className={styles.card}>
+                    <h3 className={styles.title}>Plan <strong>BUSINESS</strong></h3>
+                    <p className={styles.description}>To your company</p>
+
+                    <h4 className={styles.price}>$1000</h4>
 
                     <ul className={styles.list}>
                         <li className={styles.item}>

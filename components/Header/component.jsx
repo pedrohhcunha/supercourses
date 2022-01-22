a814ac0bd53393a7e1f8ea5a2250a315050a00e0
pedrohhcunha <pedro.cunha@onfinity.com.br>
2022-01-22 13:43:35 -0300

    Header seguindo a página

--- components/Header/component.jsx
+++ components/Header/component.jsx
@@ -1,12 +1,23 @@
 import styles from './styles.module.scss'
-
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Link from 'next/link'
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
+import { useEffect, useState } from 'react';
 
 export default function Header (props){
+
+    const [animateHeader, setAnimateHeader] = useState(false);
+
+    useEffect(() => {
+        window.addEventListener("scroll", () => {
+            if (window.scrollY > 40) {
+                setAnimateHeader(true)
+            } else setAnimateHeader(false)
+        })
+    }, []);
+    
     return(
-        <header className={styles.header}>
+        <header className={`${styles.header} ${animateHeader ? styles.active : ''}`}>
             <nav className={styles.nav}>
                 <h1 className={styles.title}>Super Courses</h1>
                 <ul className={styles.list}>
